89e422b5edf552509fa50883e7fdb98b94713582
janemeharry <janemeharry@hotmail.com>
2020-08-02 07:34:54 +0100

    Updated to week 5 version

--- src/script.js
+++ src/script.js
@@ -41,6 +41,12 @@ function updateDate() {
   let year = today.getFullYear();
   formattedDate.innerHTML = `${day} ${date}th ${month} ${year}`;
 }
+function search(city) {
+  let apiKey = "cf895dab58ddca90926732862e56006d";
+  let unit = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=${apiKey}`;
+  axios.get(`${apiUrl}`).then(updateTemperature);
+}
 
 function updateCity(event) {
   event.preventDefault();
@@ -51,27 +57,56 @@ function updateCity(event) {
   } else {
     updatedCity.innerHTML = city.value;
   }
+  search(city.value);
 }
 
 function changeFarenheit(event) {
   event.preventDefault();
-  let farenheit = document.querySelector("#farenheit");
-  let celsius = document.querySelector("#celsius");
-  let celsiusTemp = document.querySelector("#celsius-temp");
-  let farenheitTemp = celsiusTemp.innerHTML * (9 / 5) + 32;
-  celsiusTemp.innerHTML = farenheitTemp;
-  farenheit.innerHTML = `°C`;
-  celsius.innerHTML = `°F`;
+  let weatherUnit = document.querySelector("#weather-unit");
+  let currentTemp = document.querySelector("#current-temp");
+  let farenheitTemp = Math.round(currentTemp.innerHTML * (9 / 5) + 32);
+  currentTemp.innerHTML = farenheitTemp;
+  weatherUnit.innerHTML = `°F`;
 }
 
 function changeCelsius(event) {
   event.preventDefault();
-  let farenheit = document.querySelector("#farenheit");
-  let celsius = document.querySelector("#celsius");
-  let celsiusTemp = document.querySelector("#celsius-temp");
-  celsiusTemp.innerHTML = 30;
-  farenheit.innerHTML = `°F`;
-  celsius.innerHTML = `°C`;
+  let weatherUnit = document.querySelector("#weather-unit");
+  let currentTemp = document.querySelector("#current-temp");
+  currentTemp.innerHTML = Math.round((currentTemp.innerHTML - 32) * (5 / 9));
+  weatherUnit.innerHTML = `°C`;
+}
+
+function updateTemperature(response) {
+  let updatedTemperature = Math.round(response.data.main.temp);
+  let temperature = document.querySelector("#current-temp");
+  let forecast = document.querySelector("#forecast-description");
+  let updatedForecast = response.data.weather[0].main;
+  let updatedCity = document.querySelector("#bold-city");
+  let apiCity = response.data.name;
+  updatedCity.innerHTML = `${apiCity}`;
+  temperature.innerHTML = `${updatedTemperature}`;
+  forecast.innerHTML = `${updatedForecast}`;
+}
+
+function updateGeolocationCity(response) {
+  let city = document.querySelector("#bold-city");
+  let updatedGeolocationCity = response.data.name;
+  city.innerHTML = `${updatedGeolocationCity}`;
+}
+
+function showPosition(position) {
+  let apiKey = "cf895dab58ddca90926732862e56006d";
+  let unit = "metric";
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=${unit}&appid=${apiKey}`;
+  axios.get(`${apiUrl}`).then(updateTemperature);
+  axios.get(`${apiUrl}`).then(updateGeolocationCity);
+}
+
+function getLocation() {
+  navigator.geolocation.getCurrentPosition(showPosition);
 }
 
 let searchEngine = document.querySelector("#search-engine");
@@ -85,3 +120,8 @@ celsius.addEventListener("click", changeCelsius);
 
 updateTime();
 updateDate();
+
+let currentLocation = document.querySelector("#geolocation");
+currentLocation.addEventListener("click", getLocation);
+
+search("New York");
